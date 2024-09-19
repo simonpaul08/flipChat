e142737873d8b34c656b18b44cae54c3605ec124
Simon Paul <simon.paul@xenonstack.com>
2024-09-19 21:13:04 +0530

    feat: landing page initialized

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import LOGO from "../assets/Flipchat-Transperent.png"
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className='landing'>
+      <div className="landing-section">
+        <div className="landing-container">
+          
+          {/* landing header */}
+          <div className="landing-header">
+            <div className="landing-header-logo">
+              <img src={LOGO} alt="flipchat-logo" className='landing-logo'/>
+            </div>
+            <div className="landing-header-nav"></div>
+            <div className="landing-header-cta"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
